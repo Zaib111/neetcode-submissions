3ba90bc5573c4143cc2f17917509972b70fc1913
Shahmeer Khan <70596616+Zaib111@users.noreply.github.com>
2026-06-23 14:08:42 -0400

    Update: remove-node-from-end-of-linked-list - submission-0

--- Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.py
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.py
@@ -6,20 +6,16 @@
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # two pointer approach with l and r pointers to find where n is located
-        dummy = ListNode(0, head)
+        dummy, right = ListNode(0, head), head
         left = dummy
-        right = head
-        # setting right pointer
-        while n > 0 and right:
+        while n > 0:
             right = right.next
             n -= 1
-        
-        # finding where the right pointer is now (where n is)
         while right:
-            right = right.next
             left = left.next
-        # left will be one behind n now so that it can move its pointer
+            right = right.next
+        
         left.next = left.next.next
 
         return dummy.next
+        
